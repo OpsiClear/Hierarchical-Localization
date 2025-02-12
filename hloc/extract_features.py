30ca6e6572e9144873bd021549397c6f1534d39e
aelhakie <aly@opsiclear.com>
2025-02-12 13:51:06 -0500

    use mask

--- hloc/extract_features.py
+++ hloc/extract_features.py
@@ -175,9 +175,10 @@ class ImageDataset(torch.utils.data.Dataset):
         "interpolation": "cv2_area",  # pil_linear is more accurate but slower
     }
 
-    def __init__(self, root, conf, paths=None):
+    def __init__(self, root, conf, paths=None, mask_dir: Optional[Path]=None):
         self.conf = conf = SimpleNamespace(**{**self.default_conf, **conf})
         self.root = root
+        self.mask_dir = mask_dir
 
         if paths is None:
             paths = []
@@ -223,6 +224,10 @@ class ImageDataset(torch.utils.data.Dataset):
             "image": image,
             "original_size": np.array(size),
         }
+        if self.mask_dir:
+            mask_path = self.mask_dir / f'{name}.png'
+            if mask_path.exists():
+                data['mask'] = read_image(mask_path, True)
         return data
 
     def __len__(self):
@@ -238,12 +243,13 @@ def main(
     image_list: Optional[Union[Path, List[str]]] = None,
     feature_path: Optional[Path] = None,
     overwrite: bool = False,
+    mask_dir: Optional[Path] = None
 ) -> Path:
     logger.info(
         "Extracting local features with configuration:" f"\n{pprint.pformat(conf)}"
     )
 
-    dataset = ImageDataset(image_dir, conf["preprocessing"], image_list)
+    dataset = ImageDataset(image_dir, conf["preprocessing"], image_list, mask_dir)
     if feature_path is None:
         feature_path = Path(export_dir, conf["output"] + ".h5")
     feature_path.parent.mkdir(exist_ok=True, parents=True)
@@ -276,6 +282,12 @@ def main(
                 pred["scales"] *= scales.mean()
             # add keypoint uncertainties scaled to the original resolution
             uncertainty = getattr(model, "detection_noise", 1) * scales.mean()
+            if 'mask' in data:
+                mask = data['mask'][0] # cuz `batch_size == 1`
+                valid_keypoint = mask[pred['keypoints'][:, 1].astype('int'), pred['keypoints'][:, 0].astype('int')]
+                pred['keypoints'] = pred['keypoints'][valid_keypoint > 0]
+                pred['descriptors'] = pred['descriptors'][:, valid_keypoint > 0]
+                pred['scores'] = pred['scores'][valid_keypoint > 0]
 
         if as_half:
             for k in pred:
@@ -317,6 +329,7 @@ if __name__ == "__main__":
     parser.add_argument("--as_half", action="store_true")
     parser.add_argument("--image_list", type=Path)
     parser.add_argument("--feature_path", type=Path)
+    parser.add_argument("--mask_dir", type=Path)
     args = parser.parse_args()
     main(
         confs[args.conf],
@@ -325,4 +338,5 @@ if __name__ == "__main__":
         args.as_half,
         args.image_list,
         args.feature_path,
+        args.mask_dir,
     )
